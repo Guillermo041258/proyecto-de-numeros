37d1e60a96b0e682eceb3ec6a62807a5316614a2
Guillermo <Vargasgjoaquin003@outlook.com>
2018-12-17 13:40:17 -0600

    proyecto de  numeros

---
+++ src/edex/Edex.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edex;
+ import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class Edex {
+
+
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+        
+        }
+
+    
+    
+   
+ 
+    
+}
+     public class MyRegex 
+    {
+     public static String MyRegex(String ip)
+     {
+         
+         return rgex;
+     }
+     
+     }
+}
+
